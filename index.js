4637a8d21f0664a6b9fa5c853648eaae0ace21ed
unknown <glebtverd@gmail.com>
2022-12-13 22:49:04 +0300

    change index.js

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+require("dotenv").config()
+const express = require("express");
+const sequelize = require("./db");
+const fileUpload = require('express-fileupload')
+const cors = require("cors");
+const path = require('path');
+const models = require("./models/model");
+const router = require("./routers/index");
+const {ErrorHandler} = require("./middleware/errorHandlerMiddleware");
+const app = express();
+
+const Port = process.env.PORT || 8080
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
+app.use('/', router)
+app.use(ErrorHandler)
+
+app.get('/', (req, res) => res.json("hellow world"))
+
+const start = async () => {
+	try{
+		await sequelize.authenticate();
+		await sequelize.sync();
+		app.listen(Port, () => console.log(`server started on port ${Port}`));
+	}
+	catch(e){
+		console.log(e);
+	}
+}
+
+start()
